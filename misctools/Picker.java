ffccf5bac6c5d084031a5aa7440c1565833a1dc6
waflores <waflores@ncsu.edu>
2012-08-31 14:13:00 -0400

    Confirming the transfer of files to new organized project.

---
+++ misctools/Picker.java
@@ -0,0 +1,35 @@
+package misctools;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import helpertools.*;
+import javax.swing.*;
+
+
+public class Picker implements ActionListener {
+	/* GUI Stuff */
+	private JButton b = new JButton("popup");
+	private JFrame f = new JFrame();
+	private JTextField text = new JTextField(20);
+	
+	 public static void main(String[] args) {
+         new Picker();
+	 }
+	 
+	 /* Picker Constructor */
+	 public Picker(){
+		 JLabel label = new JLabel("Selected Date:");
+         JPanel p = new JPanel();
+         p.add(label);
+         p.add(text);
+         p.add(b);
+         f.getContentPane().add(p);
+         f.pack();
+         f.setVisible(true);
+         b.addActionListener(this);
+         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 }
+	 
+	  public void actionPerformed(ActionEvent ae) {
+		  if (ae.getSource() == b) text.setText(new DatePicker(f).setPickedDate());
+  }
+}
